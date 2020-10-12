e9788b663ad438bbff2d870d833292d181d1d588
Manoj <manojd@rgmobiles.com>
2020-10-13 00:46:34 +0530

    js prevendefalut,loval,seession

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+componentDidMount(){
+  localStorage.setItem("name","John Doe");
+  sessionStorage.setItem("name","mark");
+
+
 }
+  funJs=(event)=>{
+    event.preventDefault();
+  }
 
-export default App;
+  herfAdd=(event)=>{
+event.preventDefault();
+  }
+  render() {
+    return (
+      <div className="App">
+      <h1>JS value propertity......!</h1>
+<form action="index.html">
+  <label>
+   Enter your name
+  </label>
+  <input 
+    type="text" id="i"/>
+  <br></br>
+  <button onClick={this.funJs} type="submit">submit</button>
+</form>
+<a href="www.google.com" onClick={this.herfAdd}>google</a>
+<h3>localStorage....!</h3>
+<h3>{localStorage.getItem("name")}</h3>
+<h3>sessionStorage....!</h3>
+<h3>{sessionStorage.getItem("name")}</h3>
+    </div>
+   
+    )
+  }
+}
